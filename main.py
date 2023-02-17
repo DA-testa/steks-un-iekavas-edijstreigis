38662a69d0399c834077cefaa0073c563713a354
Edijs <125146906+edijstreigis@users.noreply.github.com>
2023-02-17 09:39:14 +0000

    TRY

--- main.py
+++ main.py
@@ -14,21 +14,20 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i + 1))
-            pass
 
         if next in ")]}":
-            if not opening_brackets_stack:return i + 1
+            if not opening_brackets_stack:
+                return i + 1
             if not are_matching((opening_brackets_stack.pop()).char,next):
                 return i + 1
-            pass
-    if opening_brackets_stack:return opening_brackets_stack[0].possition
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].possition
     return"Succes"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    if "I" in text: print(find_mismatch(input()))
 
 if __name__ == "__main__":
     main()
