82eb2a366cf31cc78b9d1a9f6040ef77573d27c4
Edijs <125146906+edijstreigis@users.noreply.github.com>
2023-02-17 09:52:47 +0000

    Edijs Treiģis

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def find_mismatch(text):
                 return i + 1
     if opening_brackets_stack:
         return opening_brackets_stack[0].possition
-    return"Succes"
+    return"Success"
 
 
 def main():
